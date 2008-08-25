9819c941ced1ea9dc30d9f1045501aa490ae393d
per.malmen <per.malmen@02195dbe-a02b-0410-ae09-db10261af934>
2008-08-25 15:03:42 +0000

    Removed the feature to expose constructors

--- src/se/krka/kahluax/annotation/LuaJavaClassFactory.java
+++ src/se/krka/kahluax/annotation/LuaJavaClassFactory.java
@@ -40,7 +40,6 @@ public class LuaJavaClassFactory {
 
 		//System.out.println("Registering class: " + name);
 
-
 		List<Class> hierchy = new ArrayList<Class>();
 		Class superClass = clazz.getSuperclass();
 		while (!superClass.equals(Object.class)) {
@@ -71,8 +70,8 @@ public class LuaJavaClassFactory {
 	private ClassHolder getClassHolder(String name, Class clazz, boolean exposeConstructor, ClassHolder superClass) {
 		ClassHolder holder = classMap.get(clazz);
 		if(holder == null) {
-			System.out.println("Adding to classmap " + clazz);
-			holder = new ClassHolder(name, clazz, exposeConstructor, superClass);
+			//System.out.println("Adding to classmap " + clazz);
+			holder = new ClassHolder(clazz, superClass);
 			classMap.put(clazz, holder);
 		}
 		return holder;
@@ -82,80 +81,6 @@ public class LuaJavaClassFactory {
 		String name = getName(clazz);
 		ClassHolder ch = classMap.get(clazz);
 		ch.it.rawset(methodName, javaFunction);
-	}
-
-
-	public void registerFactory() {
-		state.environment.rawset("createClassInstance", new JavaFunction() {
-
-			public int call(LuaCallFrame luaCallFrame, int i) {
-				BaseLib.luaAssert(i > 1, "Not enugh params");
-				String name = (String) luaCallFrame.get(0);
-
-				if (!classMap.containsKey(name)) {
-					throw new RuntimeException("Unknown class: " + name);
-				}
-
-				ClassHolder ch = classMap.get(name);
-				if (!ch.exposeConstructor) {
-					throw new RuntimeException("Cant create new instance of that class");
-				}
-
-				if (i > 1) {
-
-					Object params[] = new Object[i - 1];
-					for (int paramIndex = 1; paramIndex < i; paramIndex++) {
-						params[paramIndex - 1] = luaCallFrame.get(paramIndex);
-					}
-
-					try {
-						Object o = createInstance(name, params);
-						luaCallFrame.push(o);
-						return 1;
-					} catch (Exception e) {
-						throw new RuntimeException("Failed to create instance: " + e.getMessage());
-					}
-
-				} else {
-					try {
-						Object o = createInstance(name);
-						luaCallFrame.push(o);
-						return 1;
-					} catch (Exception e) {
-						throw new RuntimeException("Failed to create instance: " + e.getMessage());
-					}
-				}
-
-			}
-		});
-	}
-
-	private Object createInstance(String name, Object[] params) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-		if (classMap.containsKey(name)) {
-			Class clazz = classMap.get(name).clazz;
-
-			Constructor constructor = null;
-			for (Constructor c : clazz.getConstructors()) {
-				if (c.isAnnotationPresent(LuaConstructor.class)) {
-					constructor = c;
-					break;
-				}
-			}
-
-			if (constructor == null)
-				throw new InstantiationException("Cant create class, unknown constructor");
-
-
-			if (constructor.getParameterTypes().length != params.length)
-				throw new InstantiationException("Cant create class, wrong number of params");
-
-			Class[] paramTypes = constructor.getParameterTypes();
-			Object[] newParams = castParams(params, paramTypes);
-
-			return constructor.newInstance(newParams);
-
-		}
-		throw new InstantiationException("Cant create unknown class: " + name);
 	}
 
 	private Object[] castParams(Object[] params, Class[] paramTypes) {
@@ -210,20 +135,16 @@ public class LuaJavaClassFactory {
 
 
 	private class ClassHolder {
-		private String name;
-		private boolean exposeConstructor;
 		private Class clazz;
 		LuaTable mt;
 		LuaTable it;
 
-		private ClassHolder(String name, Class clazz, boolean exposeConstructor) {
-			this(name, clazz, exposeConstructor, null);
+		private ClassHolder(Class clazz) {
+			this(clazz, null);
 		}
 
-		private ClassHolder(String name, Class clazz, boolean exposeConstructor, ClassHolder superClass) {
-			this.name = name;
+		private ClassHolder(Class clazz, ClassHolder superClass) {
 			this.clazz = clazz;
-			this.exposeConstructor = exposeConstructor;
 
 			mt = new LuaTable();
 			it = new LuaTable();
@@ -236,8 +157,7 @@ public class LuaJavaClassFactory {
 
 
 		private void registerLuaMethod(String methodName, final Method method) {
-
-			System.out.println("Registering method: " + methodName + " of " + clazz);
+			//System.out.println("Registering method: " + methodName + " of " + clazz);
 
 			it.rawset(methodName, new JavaFunction() {
 				public int call(LuaCallFrame luaCallFrame, int i) {
