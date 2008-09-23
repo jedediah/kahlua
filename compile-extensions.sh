58836e25867f1519e3dd4af977a125e95be431fc
kristofer.karlsson <kristofer.karlsson@02195dbe-a02b-0410-ae09-db10261af934>
2008-09-23 12:05:48 +0000

    started cleanup of organization

--- compile-extensions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-find src | grep "\.java$" | xargs javac -d bin -target 1.5 -source 1.5
-
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd tests
-make && java -cp ../bin se.krka.kahlua.test.Test bin $1
-cd ..
