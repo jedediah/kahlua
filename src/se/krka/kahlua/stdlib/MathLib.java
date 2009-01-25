825e24b8a53f599cbe3f5bdc150ea546b04c13be
kristofer.karlsson <kristofer.karlsson@02195dbe-a02b-0410-ae09-db10261af934>
2009-01-25 19:43:45 +0000

    fixed bug with rounding when x = 0

--- src/se/krka/kahlua/stdlib/MathLib.java
+++ src/se/krka/kahlua/stdlib/MathLib.java
@@ -214,6 +214,9 @@ public final class MathLib implements JavaFunction {
 	}
 	
 	public static double roundToSignificantNumbers(double x, int precision) {
+		if (x == 0) {
+			return 0;
+		}
 		if (x < 0) {
 			return -roundToSignificantNumbers(-x, precision);
 		}
